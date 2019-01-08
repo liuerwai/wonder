4e50b29b0f24a81ace0404fd6d4c943d92db4393
刘循光 <1069899238@qq.com>
2019-01-08 22:42:49 +0800

    修改代码

---
+++ src/main/java/com/liuxg/wonder/dao/IModelDao.java
@@ -0,0 +1,14 @@
+package com.liuxg.wonder.dao;
+
+import com.liuxg.wonder.po.Model;
+
+import java.util.List;
+
+public interface IModelDao {
+
+    List<Model> query();
+
+    Model queryOne(String id);
+
+    int add(Model model) throws Exception;
+}
---
+++ src/main/java/com/liuxg/wonder/service/IModelService.java
@@ -0,0 +1,14 @@
+package com.liuxg.wonder.service;
+
+import com.liuxg.wonder.po.Model;
+
+import java.util.List;
+
+public interface IModelService {
+
+    List<Model> query();
+
+    Model queryOne(String id);
+
+    int add(Model model) throws Exception;
+}
