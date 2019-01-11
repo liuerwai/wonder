705987ffbecb727ff6d0142644a6bd412ae29a0e
刘循光 <1069899238@qq.com>
2019-01-11 21:33:17 +0800

    压缩图片

--- src/main/java/com/liuxg/wonder/util/ImageUtils.java
+++ src/main/java/com/liuxg/wonder/util/ImageUtils.java
@@ -1,68 +1,17 @@
 package com.liuxg.wonder.util;
 
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGImageEncoder;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 
 public class ImageUtils {
-
-    /**
-     * 指定图片宽度和高度和压缩比例对图片进行压缩
-     *
-     * @param imgsrc     源图片地址
-     * @param imgdist    目标图片地址
-     * @param widthdist  压缩后图片的宽度
-     * @param heightdist 压缩后图片的高度
-     * @param rate       压缩的比例
-     */
-    public static void reduceImg(String imgsrc, String imgdist, int widthdist, int heightdist, Float rate) {
-        try {
-            File srcfile = new File(imgsrc);
-            // 检查图片文件是否存在
-            if (!srcfile.exists()) {
-                System.out.println("文件不存在");
-            }
-            // 如果比例不为空则说明是按比例压缩
-            if (rate != null && rate > 0) {
-                //获得源图片的宽高存入数组中
-                int[] results = getImgWidthHeight(srcfile);
-                if (results == null || results[0] == 0 || results[1] == 0) {
-                    return;
-                } else {
-                    //按比例缩放或扩大图片大小，将浮点型转为整型
-                    widthdist = (int) (results[0] * rate);
-                    heightdist = (int) (results[1] * rate);
-                }
-            }
-            // 开始读取文件并进行压缩
-            Image src = ImageIO.read(srcfile);
-
-            // 构造一个类型为预定义图像类型之一的 BufferedImage
-            BufferedImage tag = new BufferedImage((int) widthdist, (int) heightdist, BufferedImage.TYPE_INT_RGB);
-
-            //绘制图像  getScaledInstance表示创建此图像的缩放版本，返回一个新的缩放版本Image,按指定的width,height呈现图像
-            //Image.SCALE_SMOOTH,选择图像平滑度比缩放速度具有更高优先级的图像缩放算法。
-            tag.getGraphics().drawImage(src.getScaledInstance(widthdist, heightdist, Image.SCALE_SMOOTH), 0, 0, null);
-
-            //创建文件输出流
-            FileOutputStream out = new FileOutputStream(imgdist);
-            //将图片按JPEG压缩，保存到out中
-            JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
-            encoder.encode(tag);
-            //关闭文件输出流
-            out.close();
-        } catch (Exception ef) {
-            ef.printStackTrace();
-        }
-    }
 
     /**
      * 获取图片宽度和高度
@@ -79,8 +28,8 @@ public class ImageUtils {
             is = new FileInputStream(file);
             // 从流里将图片写入缓冲图片区
             src = ImageIO.read(is);
-            result[0] = src.getWidth(null); // 得到源图片宽
-            result[1] = src.getHeight(null);// 得到源图片高
+            result[0] = src.getWidth(null);
+            result[1] = src.getHeight(null);
             is.close();  //关闭输入流
         } catch (Exception ef) {
             ef.printStackTrace();
@@ -90,18 +39,27 @@ public class ImageUtils {
     }
 
     /**
-     * 压缩图片（等宽，等高）
+     * 压缩图片
      *
-     * @param file
      * @throws Exception
      */
     public static void reduceImg(String file) throws Exception {
 
-        // 等宽 等高 压缩
+        File srcFile = new File(file);
+        reduceImg(file, getImgWidthHeight(srcFile)[0], getImgWidthHeight(srcFile)[1]);
+    }
+
+    /**
+     * 压缩图片至固定分辨率  填充空白
+     *
+     * @param file
+     * @throws Exception
+     */
+    public static void reduceImg(String file, int width, int height) throws Exception {
+
         File srcFile = new File(file);
         File distfile = new File(file.split("\\.")[0] + "redusTo" + file.split("\\.")[1]);
-        reduceImg(srcFile.getPath(), distfile.getPath(),
-                getImgWidthHeight(srcFile)[0], getImgWidthHeight(srcFile)[1], null);
+        scale(srcFile.getPath(), distfile.getPath(), width, height);
         // 删除原图片
         Files.deleteIfExists(srcFile.toPath());
         // 压缩后的图片改名为原图片名字
@@ -109,10 +67,56 @@ public class ImageUtils {
         Files.deleteIfExists(distfile.toPath());
     }
 
+
+    /**
+     * 缩放图片方法
+     *
+     * @param srcPath 要缩放的图片路径
+     * @param result  缩放后的图片路径
+     * @param height  目标高度像素
+     * @param width   目标宽度像素
+     */
+    public final static void scale(String srcPath, String result, int width, int height) {
+
+        try {
+            BufferedImage bufferedImage = ImageIO.read(new File(srcPath));
+            int srcWidth = bufferedImage.getWidth(null);
+            int srcHeight = bufferedImage.getHeight(null);
+            Image itemp = bufferedImage.getScaledInstance(srcWidth, srcHeight, bufferedImage.SCALE_SMOOTH);
+            // 如果源图片大于目的图片 先将源图片缩小
+            if (srcWidth > width || srcHeight > height) {
+                // 取较小的比例
+                double ratioHeight = Double.valueOf(height) / srcHeight;
+                double ratioWhidth = Double.valueOf(width) / srcWidth;
+                double ratio = ratioHeight < ratioWhidth ? ratioHeight : ratioWhidth;
+                AffineTransformOp op = new AffineTransformOp(AffineTransform//仿射转换
+                        .getScaleInstance(ratio, ratio), null);//返回表示剪切变换的变换
+                itemp = op.filter(bufferedImage, null);//转换源 BufferedImage 并将结果存储在目标 BufferedImage 中。
+            }
+            // 将小的源图片填充到大的纯白的目的图片中
+
+            BufferedImage image = new BufferedImage(width, height,
+                    BufferedImage.TYPE_INT_RGB);
+            Graphics2D g = image.createGraphics();
+            g.setColor(Color.white);
+            g.fillRect(0, 0, width, height);
+            // 再将源图片填充在白色图片中央
+            g.drawImage(itemp, (width - itemp.getWidth(null)) / 2, (height - itemp.getHeight(null)) / 2,
+                    itemp.getWidth(null), itemp.getHeight(null),
+                    Color.white, null);
+            g.dispose();
+            itemp = image;
+
+            ImageIO.write((BufferedImage) itemp, "JPEG", new File(result));      //输出压缩图片
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws Exception {
 
-        reduceImg("C:\\Users\\liuxu\\Desktop\\home.jpg");
-
+        reduceImg("C:\\Users\\liuxu\\Desktop\\4.jpg");
     }
+
 
 }
